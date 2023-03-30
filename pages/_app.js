09dba1c5823c2d4bf21239579d1ad2f523a4b5ea
saba faiz <sabafaiz.ind@gmail.com>
2023-03-31 00:47:10 +0530

    first-blog-on-next-js

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import styles from '../styles/globals.css'
+
+export default function App({Component , pageProps}){
+    return(
+        <div>
+            <Component {...pageProps} />
+        </div>
+    )
+
+}
